6ca29fa94ce539bd14f977d281ef8dc7ba9305fd
Mike Melanson <mike@multimedia.cx>
2023-03-09 21:38:02 -0800

    Create proper command line options
    Instead of requiring rigid command line argument ordering, allow for
    proper command line options. Also, make rate limiting optional.

--- dl-ia-item.sh
+++ dl-ia-item.sh
@@ -1,7 +1,39 @@
-LIMIT_RATE=$1
-ITEM=$2
+#!/bin/bash
 
+RATE_LIMIT=""
+
+# check that the 'ia' tool is available
+if ! command -v ia &> /dev/null
+then
+    echo "'ia' command could not be found"
+    exit 1
+fi
+
+# parse arguments
+while getopts 'r:i:h' opt; do
+    case "$opt" in
+        r)
+            RATE_LIMIT="--limit-rate=$OPTARG"
+            echo $RATE_LIMIT
+            ;;
+        i)
+            ITEM=$OPTARG
+            echo $ITEM
+            ;;
+        h)
+            echo "Usage: $(basename $0) [-r rate_limit] -i ia_item_id"
+            ;;
+    esac
+done
+
+# ensure that an item ID was specified
+if [ -z "${ITEM}" ]; then
+    echo "no item ID specified; exiting"
+    exit 1
+fi
+
+# get the list of download URLs via the ia tool
 for file in $(ia download --dry-run ${ITEM})
 do
-  wget --recursive --limit-rate=$LIMIT_RATE --continue --no-host-directories --cut-dirs=1 $file
+    wget --recursive ${RATE_LIMIT} --continue --no-host-directories --cut-dirs=1 $file
 done
